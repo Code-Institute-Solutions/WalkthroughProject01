b63dee4086c96fbd7b42ef1459e4ffbf624c56f6
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-08-26 17:44:28 +0000

    update

--- app_pages/page_malaria_detector.py
+++ app_pages/page_malaria_detector.py
@@ -15,6 +15,7 @@ def page_page_malaria_detector_body():
     st.write("---")
     if img_file_buffer is not None:
         img = np.array((Image.open(img_file_buffer)))
+        st.write("* Blood Smear Sample")
         st.image(img, caption=f"{img.shape}")
         
         
@@ -27,23 +28,30 @@ def page_page_malaria_detector_body():
         pred_proba = model.predict(my_image)[0,0]
 
 
-        target_map = {'Parasitized':1, 'Uninfected':0} # {v: k for k, v in {'Parasitized':1, 'Uninfected':0}}
+        target_map = {v: k for k, v in {'Parasitized': 0, 'Uninfected': 1}.items()}
+        
         pred_class =  target_map[pred_proba > 0.5]  
+        st.write(f"* The predictive analysis indicates the cell is {pred_class.lower()} with Malaria.")
 
-        # if pred_class == target_map[0]: pred_proba = 1 - pred_proba
-        # st.write(target_map,pred_class)
+        if pred_class == target_map[0]: pred_proba = 1 - pred_proba
 
+        import pandas as pd
+        prob_per_class= pd.DataFrame(data=[0,0],index={'Parasitized': 0, 'Uninfected': 1}.keys(), columns=['Probability'])
 
-        # prob_per_class= pd.DataFrame(data=[0,0],index=train_image_gen.class_indices.keys(), columns=['Probability'])
+        prob_per_class.loc[pred_class] = pred_proba
 
-        # prob_per_class.loc[pred_class] = pred_proba
+        import plotly.express as px
+        for x in prob_per_class.index.to_list():
+            if x not in pred_class: prob_per_class.loc[x] = 1 - pred_proba
 
-        # for x in prob_per_class.index.to_list():
-        #     if x not in pred_class: prob_per_class.loc[x] = 1 - pred_proba
+        prob_per_class = prob_per_class.round(3)
+        prob_per_class['Diagnostic'] = prob_per_class.index
+        import plotly.express as px
+        fig = px.bar(
+                prob_per_class,
+                x = 'Diagnostic',
+                y = prob_per_class['Probability'],
+                range_y=[0,1],
+                width=600, height=400,template='presentation')
 
-        # prob_per_class = prob_per_class.round(3)
-        # print(prob_per_class)
-        # import plotly.express as px
-        # fig = px.bar(prob_per_class, x = prob_per_class.index, y = prob_per_class['Probability'],range_y=[0,1],
-        #             labels=dict(x="Diagnosis"), width=400, height=500)
-        # st.plotly_chart(fig)
+        st.plotly_chart(fig)
