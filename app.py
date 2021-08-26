1791d10663afeb0631f2a4fba87afbb81bd78fad
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-08-26 10:58:36 +0000

    update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.page_summary import page_summary_body
 from app_pages.page_cells_visualizer import page_cells_visualizer_body
+from app_pages.page_malaria_detector import page_page_malaria_detector_body
 
 # Create an instance of the app 
 app = MultiPage() 
@@ -11,6 +12,8 @@ app = MultiPage()
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Cells Visualizer", page_cells_visualizer_body)
+app.add_page("Malaria Detection", page_page_malaria_detector_body)
+
 
 # Run the  app
 app.run(app_name= "Malaria Detector")
---
+++ app_pages/page_malaria_detector.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def page_page_malaria_detector_body():
+    st.write("### page_malaria_detector")
+    # st.info(
