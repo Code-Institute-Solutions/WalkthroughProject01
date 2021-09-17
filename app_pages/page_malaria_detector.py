334840b1bed9a3d0a14b10961f44855df3af1ef6
FernandoRocha88 <fernando.doreto88@gmail.com>
2021-09-17 22:48:35 +0000

    update

--- app_pages/page_malaria_detector.py
+++ app_pages/page_malaria_detector.py
@@ -37,18 +37,19 @@ def page_malaria_detector_body():
         df_report = pd.DataFrame([])
         for image in images_buffer:
 
-            img = np.array((Image.open(image)))
+            img_pil = (Image.open(image))
             st.info(f"Blood Smear Sample: **{image.name}**")
-            
-            st.image(img, caption=f"Image Size: {img.shape[1]}px width x {img.shape[0]}px height")
-            resized_img = resize_input_image(img)
+            img_array = np.array(img_pil)
+            st.image(img_pil, caption=f"Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
 
-            pred_proba, pred_class = load_model_and_predict(resized_img)
+            version = 'v1'
+            resized_img = resize_input_image(img=img_pil, version=version)
+            pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
             plot_predictions_probabilities(pred_proba, pred_class)
+
             df_report = df_report.append({"Name":image.name, 'Result': pred_class },
                                         ignore_index=True)
         
-
         if not df_report.empty:
             st.success("Analysis Report")
             st.table(df_report)
--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.express as px
+from src.data_management import load_pkl_file
 
 
 def plot_predictions_probabilities(pred_proba, pred_class):
@@ -27,26 +28,30 @@ def plot_predictions_probabilities(pred_proba, pred_class):
 
 
 
-import cv2      
-def resize_input_image(img):        
-    # img_resized = cv2.resize(img,(132,133)) 
-    img_resized = cv2.resize(img,(130,130)) 
+from PIL import Image
+def resize_input_image(img, version):  
+    image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
+    img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
     my_image = np.expand_dims(img_resized, axis=0)
+
     return my_image
 
 
 
 from tensorflow.keras.models import load_model
-def load_model_and_predict(my_image):
+def load_model_and_predict(my_image, version):
+
     # model = load_model('outputs/model/my_model.h5')
-    model = load_model('outputs/model/malaria_detector.h5')
+    # model = load_model(f"outputs/model/malaria_detector.h5")
+    model = load_model(f"outputs/{version}/malaria_detector_model.h5")
+
     pred_proba = model.predict(my_image)[0,0]
 
     target_map = {v: k for k, v in {'Parasitized': 0, 'Uninfected': 1}.items()}
     pred_class =  target_map[pred_proba > 0.5]  
     if pred_class == target_map[0]: pred_proba = 1 - pred_proba
 
-    # st.write(pred_class,pred_proba)
+
     st.write(
         f"The predictive analysis indicates the sample cell is "
         f"**{pred_class.lower()}** with malaria.")
