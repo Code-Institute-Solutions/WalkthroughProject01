ef36b321fce1fb5edb60cb608ed69f73f3052fa8
Gyan Shashwat <gyan.shashwat@ucdconnect.ie>
2021-11-24 16:13:40 +0000

    changed kaggle URL

--- app_pages/page_malaria_detector.py
+++ app_pages/page_malaria_detector.py
@@ -18,7 +18,7 @@ def page_malaria_detector_body():
 
     st.write(
         f"* You can download a set of parasitized and uninfected cells for live prediction. "
-        f"You can download the images from [here](https://www.kaggle.com/gyanshashwat1611/cell-images-test)"
+        f"You can download the images from [here](https://www.kaggle.com/codeinstitute/cell-images-test)"
         )
 
     st.write("---")
