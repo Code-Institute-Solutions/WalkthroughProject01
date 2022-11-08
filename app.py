a4f7e832cc8213079d9f0d59be51d0a4077524b1
NielMc <niel@codeinstitute.net>
2022-11-08 16:22:13 +0000

    pep8 code

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from app_pages.page_malaria_detector import page_malaria_detector_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
 from app_pages.page_ml_performance import page_ml_performance_metrics
 
-app = MultiPage(app_name= "Malaria Detector") # Create an instance of the app 
+app = MultiPage(app_name="Malaria Detector")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Cells Visualizer", page_cells_visualizer_body)
+app.add_page("Cells Visualiser", page_cells_visualizer_body)
 app.add_page("Malaria Detection", page_malaria_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
-app.run() # Run the  app
+app.run()  # Run the app
--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -13,22 +13,23 @@ def plot_predictions_probabilities(pred_proba, pred_class):
     """
 
     prob_per_class = pd.DataFrame(
-            data=[0, 0],
-            index={'Parasitized': 0, 'Uninfected': 1}.keys(),
-            columns=['Probability']
-        )
+        data=[0, 0],
+        index={'Parasitised': 0, 'Uninfected': 1}.keys(),
+        columns=['Probability']
+    )
     prob_per_class.loc[pred_class] = pred_proba
     for x in prob_per_class.index.to_list():
-        if x not in pred_class: prob_per_class.loc[x] = 1 - pred_proba
+        if x not in pred_class:
+            prob_per_class.loc[x] = 1 - pred_proba
     prob_per_class = prob_per_class.round(3)
     prob_per_class['Diagnostic'] = prob_per_class.index
 
     fig = px.bar(
-            prob_per_class,
-            x='Diagnostic',
-            y=prob_per_class['Probability'],
-            range_y=[0, 1],
-            width=600, height=300, template='seaborn')
+        prob_per_class,
+        x='Diagnostic',
+        y=prob_per_class['Probability'],
+        range_y=[0, 1],
+        width=600, height=300, template='seaborn')
     st.plotly_chart(fig)
 
 
@@ -54,7 +55,8 @@ def load_model_and_predict(my_image, version):
 
     target_map = {v: k for k, v in {'Parasitized': 0, 'Uninfected': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
-    if pred_class == target_map[0]: pred_proba = 1 - pred_proba
+    if pred_class == target_map[0]:
+        pred_proba = 1 - pred_proba
 
     st.write(
         f"The predictive analysis indicates the sample cell is "
